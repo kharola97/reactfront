e07fcf0cc83e9833d5b4708be5da5b4649246096
kharola97 <akharola4@gmail.com>
2023-10-15 00:45:33 +0530

    regiteragain

--- src/Component/Register.js
+++ src/Component/Register.js
@@ -1,36 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from "../Images/registerpage.jpg"
+import {AiFillEyeInvisible, AiFillEye} from "react-icons/ai"
 import "./Register.css"
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username:"",
+    password:"",
+    phone:"",
+    email:""
+  })
+
+  const [visible, setVisible] = useState(false)
+
+  const handleInputChange =  (e)=>{
+         const {name, value} = e.target;
+         setFormData({...formData,[name]:value})
+  }
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log(formData)
+  }
   return (
     <div className='Register'>
       <div className='right-image'><img src={Image} alt='empty'></img></div>
-      <div className='left-register'> <form> 
+      <div className='left-register'> <form onSubmit={handleSubmit}> 
         <div> <h2> Create an account </h2></div>
         <div className='registration-form'>
           <div className='fullname'>
-          <label for="username">Username</label>
-          <input type='text' name='username' required id='username'></input>
+          <label htmlFor="username">Username</label>
+          <input type='text' name='username' required id='username' value={formData.username}onChange={handleInputChange}></input>
           </div>
           <div className='password'>
-          <label for="password">Password</label>
-          <input type='password' name='password' id='password' required minLength="6"></input>
+          <label htmlFor="password">Password</label>
+          <div className='passwordShow'>
+          <input type={visible ? "text" : "password"} name='password' id='password' required minLength="6" value={formData.password}onChange={handleInputChange}></input>
+          <div className='eyeButton' onClick={()=>setVisible(!visible)}>{ visible ? <AiFillEye/> : <AiFillEyeInvisible/>}</div>
+          </div>
           </div>
           <div className='phone'>
-          <label for="phone">Phone</label>
-          <input type='tel' name='phone' required id='phone' minLength="10" maxLength="10"></input>
+          <label htmlFor="phone">Phone</label>
+          <input type='tel' name='phone' required id='phone' minLength="10" maxLength="10" value={formData.phone}onChange={handleInputChange}></input>
           </div>
           <div className='email'>
-          <label for="email">Email</label>
-          <input type='email' name='email' id='email' required ></input>
+          <label htmlFor="email">Email</label>
+          <input type='email' name='email' id='email' required value={formData.email} onChange={handleInputChange}></input>
+          
           </div>
           <div className='register-button'>
-            <button type='submit'>Register</button>
+            <button type='submit' >Register</button>
           </div>
         </div>
         </form>
-        <p>Already have an account? <a href='#' alt="">Login</a></p>
+        <p className='form-footer'>Already have an account? <a href='#' alt="">Login</a></p>
         </div>
+        
     </div>
   )
 }
